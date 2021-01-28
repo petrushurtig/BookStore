aa873216aac6c7176adc509d170f057f9aa52280
Petrus Hurtig <petrushurtig@hotmail.com>
2021-01-28 11:17:44 +0200

    Created REST services

--- src/main/java/hurtig/BookStore/web/BookController.java
+++ src/main/java/hurtig/BookStore/web/BookController.java
@@ -1,6 +1,7 @@
 package hurtig.BookStore.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hurtig.BookStore.domain.Book;
 import hurtig.BookStore.domain.BookRepository;
@@ -19,37 +21,49 @@ public class BookController {
 	
 	@Autowired
 	private BookRepository repository;
+	
 	@Autowired
 	private CategoryRepository crepository;
 	
+	//show all books
 	@RequestMapping("/booklist")
 	public String bookslist(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
-	
-	@GetMapping("/index")
-	public String bookForm(Model model) {
-		model.addAttribute("book", new Book());
-		return "books";
+	//RESTful service to get all books
+	@RequestMapping(value="/books", method= RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+	//RESTful service to get student by id
+	@RequestMapping(value="/book/{id}", method= RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+		return repository.findById(bookId);
 	}
 	
+	//Add new book
 	@RequestMapping(value="/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
 		model.addAttribute("categorys", crepository.findAll());
 		return "addBook";
 	}
+	
+	//Save update book
 	@RequestMapping(value="/edit/{id}", method= RequestMethod.GET)
 	public String editBook(@PathVariable("id") Long Id, Model model) {
 		model.addAttribute("book", repository.findById(Id));
 		return "editBook";
 	}
+	
+	//Save new book
 	@RequestMapping(value="/save", method= RequestMethod.POST)
 	public String save(Book book) {
 		repository.save(book);
 		return "redirect:booklist";
 	}
+	//Delete book
 	@RequestMapping(value= "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long Id, Model model) {
 		repository.deleteById(Id);
