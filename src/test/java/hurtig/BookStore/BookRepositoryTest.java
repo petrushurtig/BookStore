0a24c2e388faaee823e6ad4bdcef82fc697fef2b
Petrus Hurtig <petrushurtig@hotmail.com>
2021-02-06 10:37:38 +0200

    added test cases

---
+++ src/test/java/hurtig/BookStore/BookRepositoryTest.java
@@ -0,0 +1,39 @@
+package hurtig.BookStore;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import hurtig.BookStore.domain.Book;
+import hurtig.BookStore.domain.BookRepository;
+import hurtig.BookStore.domain.Category;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class BookRepositoryTest {
+
+	@Autowired
+	private BookRepository repository;
+	
+	@Test
+	public void findByTitleShouldReturnBook() {
+		List<Book> books = repository.findByTitle("Tuntematon sotilas");
+		
+		assertThat(books).hasSize(1);
+		assertThat(books.get(0).getAuthor()).isEqualTo("Väinö Linna");
+	}
+	
+	@Test
+	public void createNewBook() {
+		Book book = new Book("Älä käy yöhön yksin", "Kjell Westö", 2010, "145876-876", 20.99, new Category("Draama"));
+		repository.save(book);
+		assertThat(book.getId()).isNotNull();
+	}
+	
+}
--- src/test/java/hurtig/BookStore/BookStoreApplicationTests.java
+++ src/test/java/hurtig/BookStore/BookStoreApplicationTests.java
@@ -1,13 +1,25 @@
 package hurtig.BookStore;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import hurtig.BookStore.web.BookController;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class BookStoreApplicationTests {
 
+	@Autowired
+	private BookController controller;
+	
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
