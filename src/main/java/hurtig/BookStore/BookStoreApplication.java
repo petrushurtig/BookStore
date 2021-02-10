6dcb4369343ecf77c52ee454b84712f782ed611f
Petrus Hurtig <petrushurtig@hotmail.com>
2021-02-10 10:02:15 +0200

    deleted some logs

--- src/main/java/hurtig/BookStore/BookStoreApplication.java
+++ src/main/java/hurtig/BookStore/BookStoreApplication.java
@@ -30,14 +30,8 @@ public class BookStoreApplication {
 	@Bean
 	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
-	log.info("save a couple of books");
-	crepository.save(new Category("History"));
-	repository.save(new Book("Da Vinci -koodi", "Dan Brown", 2003, "123-21321", 20.00, crepository.findByName("History").get(0)));
 	
-	Users user1 = new Users("user", "$2a$10$ikpsGY/lLQ/QmLijqYgAVO1iiS9E6HRkZ6OGeGXNevxlW.ad/O0Qa", "USER");
-	Users user2 = new Users("admin", "$2a$10$z1J4xi1EA2ttoROyyzjpjOn4Mia8AHercTgjHionZVTjXy7HopoQO", "ADMIN");
-	urepository.save(user1);
-	urepository.save(user2);
+	
 		};
 	}
 
